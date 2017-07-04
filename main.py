4c22b5b0561df79246370ee0cb2f8f81dbae50eb
parasmriu <pjha46@gmail.com>
2017-07-04 20:48:51 +0530

    successfully fetch the most recent post of a user and download it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import requests
+import requests, urllib
 
-APP_ACCESS_TOKEN = '1471045833.68a1cf7.c126d9a38536493c97486bc8f8b3879a'
+APP_ACCESS_TOKEN = '5629236876.1cc9688.86db895c038043b5960dc2949785299a'
 #Token Owner : AVinstaBot.main
 #Sandbox Users : AVinstaBot.test0, AVinstaBot.test1, AVinstaBot.test2...... AVinstaBot.test10
 
@@ -9,6 +9,8 @@ BASE_URL = 'https://api.instagram.com/v1/'
 '''
 Function declaration to get your own info
 '''
+
+
 def self_info():
     request_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
@@ -25,9 +27,11 @@ def self_info():
     else:
         print 'Status code other than 200 received!'
 
+
 '''
 Function declaration to get the ID of a user by username
 '''
+
 
 def get_user_id(insta_username):
     request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
@@ -43,9 +47,11 @@ def get_user_id(insta_username):
         print 'Status code other than 200 received!'
         exit()
 
+
 '''
 Function declaration to get the info of a user by username
 '''
+
 
 def get_user_info(insta_username):
     user_id = get_user_id(insta_username)
@@ -67,6 +73,55 @@ def get_user_info(insta_username):
     else:
         print 'Status code other than 200 received!'
 
+
+'''
+Function declaration to get your recent post
+'''
+
+
+def get_own_post():
+    request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    own_media = requests.get(request_url).json()
+
+    if own_media['meta']['code'] == 200:
+        if len(own_media['data']):
+            image_name = own_media['data'][0]['id'] + '.jpeg'
+            image_url = own_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+
+'''
+Function declaration to get the recent post of a user by username
+'''
+
+
+def get_user_post(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            image_name = user_media['data'][0]['id'] + '.jpeg'
+            image_url = user_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+
 def start_bot():
     while True:
         print '\n'
@@ -74,42 +129,42 @@ def start_bot():
         print 'Here are your menu options:'
         print "a.Get your own details\n"
         print "b.Get details of a user by username\n"
-        #print "c.Get your own recent post\n"
-        #print "d.Get the recent post of a user by username\n"
-        #print "e.Get a list of people who have liked the recent post of a user\n"
-        #print "f.Like the recent post of a user\n"
-        #print "g.Get a list of comments on the recent post of a user\n"
-        #print "h.Make a comment on the recent post of a user\n"
-        #print "i.Delete negative comments from the recent post of a user\n"
+        print "c.Get your own recent post\n"
+        print "d.Get the recent post of a user by username\n"
+        # print "e.Get a list of people who have liked the recent post of a user\n"
+        # print "f.Like the recent post of a user\n"
+        # print "g.Get a list of comments on the recent post of a user\n"
+        # print "h.Make a comment on the recent post of a user\n"
+        # print "i.Delete negative comments from the recent post of a user\n"
         print "j.Exit"
 
-        choice=raw_input("Enter you choice: ")
-        if choice=="a":
+        choice = raw_input("Enter you choice: ")
+        if choice == "a":
             self_info()
-        elif choice=="b":
+        elif choice == "b":
             insta_username = raw_input("Enter the username of the user: ")
             get_user_info(insta_username)
-        #elif choice=="c":
-        #    get_own_post()
-        #elif choice=="d":
-        #    insta_username = raw_input("Enter the username of the user: ")
-        #    get_user_post(insta_username)
-        #elif choice=="e":
+        elif choice == "c":
+            get_own_post()
+        elif choice == "d":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_post(insta_username)
+        # elif choice=="e":
         #    insta_username = raw_input("Enter the username of the user: ")
         #    get_like_list(insta_username)
-        #elif choice=="f":
+        # elif choice=="f":
         #    insta_username = raw_input("Enter the username of the user: ")
         #    like_a_post(insta_username)
-        #elif choice=="g":
+        # elif choice=="g":
         #    insta_username = raw_input("Enter the username of the user: ")
         #    get_comment_list(insta_username)
-        #elif choice=="h":
+        # elif choice=="h":
         #    insta_username = raw_input("Enter the username of the user: ")
         #    make_a_comment(insta_username)
-        #elif choice=="i":
+        # elif choice=="i":
         #    insta_username = raw_input("Enter the username of the user: ")
         #    delete_negative_comment(insta_username)
-        elif choice=="j":
+        elif choice == "j":
             exit()
         else:
             print "wrong choice"
